c2f8f5ff50df3589b329141873dcfb7e8e8d3a5c
Schamale <Schamale@outlook.com>
2018-12-06 22:01:37 -0500

    School Lab
    Bresenham

---
+++ CIST2745-201912-Lab2.1/Assets/Scripts/TextManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+namespace CIST2745
+{
+    public class TextManager : MonoBehaviour
+    {
+        public Text X1;
+        public Text Y1;
+        public Text X2;
+        public Text Y2;
+
+        // Use this for initialization
+        public void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        public void Update()
+        {
+
+        }
+    }
+}
---
+++ Lab2.1/CIST2745-201912-Lab2.1/Assets/Line.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CIST2745
+{
+	public class Line : MonoBehaviour {
+	
+		TextManager textManager;
+	// Use this for initialization
+	void Start () {
+			textManager = FindObjectOfType<TextManager> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void DrawLine (Vector3 start, Vector3 end, Color color)
+	{
+			GameObject myLine = new GameObject ();
+			myLine.AddComponent<LineRenderer> ();
+			myLine.transform.position = start;
+			LineRenderer lr = myLine.GetComponent<LineRenderer> ();
+
+			lr.material = new Material (Shader.Find ("Particles/Alpha Blended Premultiply"));
+			lr.SetColors (color, color);
+			lr.startWidth = .8f;
+			lr.endWidth = .8f;
+			lr.SetPosition (0, start);
+			lr.SetPosition (1, end);
+		
+		
+
+	}
+}
+}
